e3ebf4f94541c6f1f505a2856476367d34647737
Hellbatx <trident6db@gmail.com>
2025-08-25 12:27:15 +0530

    Add files via upload

--- Star_Pattern.py
+++ Star_Pattern.py
@@ -2,12 +2,11 @@
 
 n= int(input("Enter the number of rows: "))
 
-for i in range(0,n-1):
+for i in range(1,n+1):
 
-    spaces= " "
-    spaces+= i
-    stars= "*"
-    stars+= n-i
+    spaces= " " * (n-i)
+    
+    stars= "*" * i
     
     print(spaces+stars)
     
